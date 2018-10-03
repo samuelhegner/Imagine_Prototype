45eb232bd44c6d71af1036ff6cc02d3822d21411
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-10-03 20:00:50 +0100

    Prototype of frest minigame oct 3

---
+++ Imagine_Protoype_Project/Assets/Forest_Touch_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest_Touch_Manager : MonoBehaviour {
+
+	public Vector2 lookPosition;
+
+	public Transform WindTrans;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		for (int i = 0; i < Input.touchCount; ++i) {
+			if (Input.GetTouch(i).phase == TouchPhase.Began) {
+				//clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+
+				lookPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+
+				
+				WindTrans.LookAt(lookPosition);
+
+			}
+		}
+	}
+}
