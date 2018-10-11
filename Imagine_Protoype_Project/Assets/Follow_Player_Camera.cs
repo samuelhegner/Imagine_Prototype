c4083ea1effc1d511c3dd74de7163756de4e8cb3
samuelhegner <C16439262@mydit.ie>
2018-10-11 13:49:22 +0100

    Cosmos basics (Samuel Hegner)

--- Imagine_Protoype_Project/Assets/Follow_Player_Camera.cs
+++ Imagine_Protoype_Project/Assets/Follow_Player_Camera.cs
@@ -8,6 +8,9 @@ public class Follow_Player_Camera : MonoBehaviour {
 
     public float cameraSpeed;
 
+    public float xOffset;
+    public float yOffset;
+
     void Awake(){
         playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
     }
@@ -20,7 +23,7 @@ public class Follow_Player_Camera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 targetPosition = new Vector3(playerPosition.position.x, playerPosition.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(playerPosition.position.x + xOffset, playerPosition.position.y + yOffset, transform.position.z);
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraSpeed);
 	}
---
+++ Imagine_Protoype_Project/Assets/Player_Movement_Cosmos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement_Cosmos : MonoBehaviour {
+
+    float playerHeight;
+    float playerStartHeight;
+    public float cameraZoomStart;
+    public float risingSpeed = 2;
+
+    public float cameraZoomSpeed;
+    public float tiltSpeed;
+
+    Camera cam;
+
+    Rigidbody2D rb;
+
+
+	void Start () {
+        playerStartHeight = transform.position.y;
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerHeight = transform.position.y;
+
+        rb.velocity = new Vector2(Input.acceleration.x * tiltSpeed, risingSpeed * Time.deltaTime);
+
+        if (cameraZoomStart <= playerHeight - playerStartHeight) {
+            print("test");
+            cam.orthographicSize += Time.deltaTime * cameraZoomSpeed;
+        }
+	}
+}
